0fc313f21915e5a898291572d4922a818e2c27b1
Dandariene Braz <dandariene@hotmail.com>
2021-01-29 20:20:10 -0300

    corrigindo código

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,28 @@ function App() {
 
   const [cep, setCep] = useState<String>("")
 
-  const [dados, setDados] = useState<Endereco>()
+  const [endereco, setEndereco] = useState<Endereco>()
 
-  const getDados = () => {
-    axios.get(`https://viacep.com.br/ws${cep}/json/`)
-      .then(resposta => setDados(resposta.data));
-
+  const getCep = () => {
+    axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(resposta => setEndereco(resposta.data));
   }
 
   return (
     <div className="App">
       <input type="text" onChange={(event) => setCep(event.target.value)} />
-      <button onClick={getDados}>Ver Endereço</button>
+      <button onClick={getCep}>Ver Endereço</button>
 
-      <h1>Endereço</h1>
+      <h1>Endereço do endereço</h1>
+      <>
+        <p>Rua: {endereco?.logradouro}</p>
+        <p>Bairro: {endereco?.bairro}</p>
+        <p>Cidade: {endereco?.localidade}</p>
+        <p>UF: {endereco?.uf}</p>
+        <p>CEP: {endereco?.cep}</p>
+        <p>DDD: {endereco?.ddd}</p>
+        <p>IBGE: {endereco?.ibge}</p>
+      </>
 
     </div>
   );
